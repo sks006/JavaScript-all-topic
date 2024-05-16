458dabd17a6e61280e3cefd86ee0465395f05def
Shihab <98661033+sks006@users.noreply.github.com>
2024-05-16 12:59:52 +0000

    browser method

---
+++ String&ItsMethod.js
@@ -0,0 +1,23 @@
+let url="https://decrypt.co/144620/bitcoin-ethereum-trade-sideways-fed-skips-rate-hike"
+
+/**
+ * =============== replace method ==============
+ */
+console.log(url.replace("-","20%"))
+
+/** 
+ * ==============  splice()  ============
+*/
+const months = ['Jan', 'March', 'April', 'June'];
+months.splice(1, 0, 'Feb');
+console.log(months);
+/**
+ * =============== Repeat method ================
+ * const mood = 'Happy! ';
+
+console.log(`I feel ${mood.repeat(3)}`);
+ Expected output: "I feel Happy! Happy! Happy! "
+
+ */
+
+console.log(`this is crypto website ${url.repeat(2)} `)
