f6ea2a83ce39c17efcb4c1e08b2c6d1c68d86013
Shihab <98661033+sks006@users.noreply.github.com>
2024-05-19 06:57:24 +0000

    math & number

---
+++ Stack&Heap.js
@@ -0,0 +1,28 @@
+//stack store premitive data copy
+/**
+ * here variable is copy in stack
+ * 
+ */
+
+let colorBrand1="black&APX"
+let shoe1=colorBrand1
+console.log(shoe1)
+
+
+let colorBrand2="black&APX"
+let shoe2="bay"
+console.log(colorBrand2)
+console.log(shoe2)
+
+/**
+ * here heap is all about referance
+ */
+
+let shihab={
+    Email:"shimkhan07@gmail.com",
+    Shoe:"bata",
+    Phone:"123456789"
+
+}
+
+console.log(shihab.Shoe)
