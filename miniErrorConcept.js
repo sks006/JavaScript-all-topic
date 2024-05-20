be74c072e2fce026e4c2f49e7359049b52fd63ca
Shihab <98661033+sks006@users.noreply.github.com>
2024-05-20 11:35:38 +0000

    5/20/2024

---
+++ miniErrorConcept.js
@@ -0,0 +1,14 @@
+//what is the value of empty object  and empty array
+
+let a={}
+if (a) {
+    console.log("this is empty obj")
+}else {
+    console.log("this is false")
+}
+let b=[]
+if (b) {
+    console.log("this is empty array")
+}else {
+    console.log("this is false")
+}
