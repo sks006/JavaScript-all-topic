4d7e34038c428ec251d16b89b9753a62b5d0a7f5
Shihab <98661033+sks006@users.noreply.github.com>
2024-05-20 04:37:14 +0000

    day_5

---
+++ Scope.js
@@ -0,0 +1,60 @@
+let a=313
+if(true){
+    s=a //a isa global scope so it can access from outside the block
+    b=34
+    return console.log(s+b)
+}
+
+
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+
+function one(){
+    const username = "shihab"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+     //console.log(website);
+
+     two()
+
+}
+
+one()
+
+if (true) {
+    const username = "shihab"
+    if (username === "shihab") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
--- object.js
+++ object.js
@@ -20,11 +20,15 @@ const JsUser = {
 // console.log(JsUser.email)
 // console.log(JsUser["email"])
 // console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
+//console.log(JsUser[mySym])
+/**
+ * object will show symbol by third bracket
+ */
+console.log(JsUser)
 
-JsUser.email = "shihab@chatgpt.com"
+//JsUser.email = "shihab@chatgpt.com"
 // Object.freeze(JsUser)
-JsUser.email = "shihab@microsoft.com"
+//JsUser.email = "shihab@microsoft.com"
 // console.log(JsUser);
 
 JsUser.greeting = function(){
@@ -36,3 +40,89 @@ JsUser.greetingTwo = function(){
 
 console.log(JsUser.greeting());
 console.log(JsUser.greetingTwo());
+
+
+// ================  ======================================
+
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "shihab",
+            lastname: "kabir"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+const obj4 = {5: "e", 6: "f"}
+console.log(obj2+obj4)//throw error
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+const obj3 = {...obj1, ...obj2}// sprad method used in obj3
+
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "shihab"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "shihab",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
