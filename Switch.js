73289675c06af6697addf59acb2a64f4218f9714
Shihab <98661033+sks006@users.noreply.github.com>
2024-05-24 05:48:43 +0000

    BMI Example with switch case

--- Switch.js
+++ Switch.js
@@ -28,3 +28,14 @@ switch (month) {
         console.log("default case match");
         break;
 }
+//! BMI with switch case.
+const BMI = (weight / ((height * height) / 10)).toFixed(2);
+results.innerHTML = `<span>${BMI}</span>`;
+
+if (BMI < 18.6) {
+    weight_guide.innerHTML = `<span>Under Weight = Less than 18.6</span>`;
+} else if (BMI >= 18.6 && BMI <= 24.9) {
+    weight_guide.innerHTML = `<span>Normal Range = 18.6 and 24.9</span>`;
+} else if (BMI > 24.9) {
+    weight_guide.innerHTML = `<span>Overweight = Greater than 24.9</span>`;
+}
