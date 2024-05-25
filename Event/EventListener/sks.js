d97f0760c124fd4a2c0d42576a18538f917b8714
Shihab <98661033+sks006@users.noreply.github.com>
2024-05-25 15:29:08 +0000

    EVENT LISTINING METHOD

---
+++ Event/EventListener/sks.js
@@ -0,0 +1,41 @@
+
+    // document.getElementById('owl').onclick = function(){
+    //     alert("owl clicked")
+    // }
+
+    TODO: attachEvent()
+    TODO: jQuery - on
+
+    TODO: type, timestamp, defaultPrevented
+    TODO: target, toElement, srcElement, currentTarget,
+    TODO; clientX, clientY, screenX, screenY
+    TODO: altkey, ctrlkey, shiftkey, keyCode
+
+    // document.getElementById('images').addEventListener('click', function(e){
+    //     console.log("clicked inside the ul");
+    // }, false)
+
+    // document.getElementById('owl').addEventListener('click', function(e){
+    //     console.log("owl clicked");
+    //     e.stopPropagation()
+    // }, false)
+    
+    // document.getElementById('google').addEventListener('click',function(e){
+    //     e.preventDefault();
+    //     e.stopPropagation()
+    //     console.log("google clicked");
+    // }, false)
+   
+    
+    document.querySelector('#images').addEventListener('click', function(e){
+        console.log(e.target.tagName);
+        if (e.target.tagName === 'IMG') {
+            console.log(e.target.id);
+            let removeIt = e.target.parentNode
+            removeIt.remove()
+        }
+    
+        
+    })
+    
+    //removeIt.parentNode.removeChild(removeIt)
