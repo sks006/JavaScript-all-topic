6e6dcf1fa34eb08472634ebdbf1898931eb89ecc
Shihab <98661033+sks006@users.noreply.github.com>
2024-05-28 09:30:03 +0000

    oop

--- advanceTopic/OOP&Class/OOP.js
+++ advanceTopic/OOP&Class/OOP.js
@@ -33,10 +33,11 @@ function User(username, loginCount, isLoggedIn){
 
 /**
  * note:
- * NEW => make a contractor or copy of User 
+ * new => make a contractor or copy of User 
  * new key use=>step1: first make a empty object/new object={}
- *              step2: contractor function call because of new keyword=every thing        *                     Encapsulation  
- *              step3: inject every thing in (this) keyword & what you encapsulate *                     previously
+ *              step2: contractor function call because of new keyword=every &* * 
+ *                     thing Encapsulation  
+ *              step3: inject every thing in (this) keyword & what you           *                     encapsulate previously
  *              step4: pass in the function
  */
 
